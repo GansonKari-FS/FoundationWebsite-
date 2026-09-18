df3b0ac8c6e9448aa24789d8dc6eda7a07c9216b
Kari Ganson <kariganson91@gmail.com>
2026-09-18 14:22:57 -0500

    fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import PageHeader from "../components/layout/PageHeader";
 
 import smile from "../assets/images/antoine-smile.jpg";
-import family from "../assets/images/antoine-allKids.jpg";
+import family from "../assets/images/antonie-allKids.jpg";
 
 function About() {
   return (
